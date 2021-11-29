a37587ad417dda5e8d7397b8489c01c0cb33cdcb
cahyo arissabarno <cahyoarissabarno@gmail.com>
2021-11-30 02:13:58 +0700

    still try to fix it

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-app.use(function (req, res, next){
-    if (req.headers['x-forwarded-proto'] === 'https') {
-      res.redirect('http://' + req.hostname + req.url);
-    } else {
-      next();
-    }
-  });
+// app.use(function (req, res, next){
+//     if (req.headers['x-forwarded-proto'] === 'https') {
+//       res.redirect('http://' + req.hostname + req.url);
+//     } else {
+//       next();
+//     }
+//   });
 const port = process.env.PORT || 3000;
 app.listen(port);
