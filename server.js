2f6148fb787658c9db3ab369e98edb7e99855db0
cahyo arissabarno <cahyoarissabarno@gmail.com>
2021-11-30 04:45:19 +0700

    fix route again

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
-// const express = require('express');
-// const serveStatic = require("serve-static")
-// const path = require('path');
-// app = express();
-// app.use(serveStatic(path.join(__dirname, 'dist')));
-// // app.use(function (req, res, next){
-// //     if (req.headers['x-forwarded-proto'] === 'https') {
-// //       res.redirect('http://' + req.hostname + req.url);
-// //     } else {
-// //       next();
-// //     }
-// //   });
-// const port = process.env.PORT || 3000;
-// app.listen(port);
-
 const express = require('express');
+const serveStatic = require("serve-static")
 const path = require('path');
 const history = require('connect-history-api-fallback');
 
-const app = express();
-
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
-
-app.use(staticFileMiddleware);
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
 app.use(history({
   disableDotRule: true,
   verbose: true
 }));
-app.use(staticFileMiddleware);
 
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
-});
+const port = process.env.PORT || 3000;
+app.listen(port);
 
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
+// const express = require('express');
+// const serveStatic = require("serve-static")
+// const path = require('path');
+// const history = require('connect-history-api-fallback');
+
+// const app = express();
+
+// const staticFileMiddleware = serveStatic(path.join(__dirname + 'dist'));
+
+// app.use(staticFileMiddleware);
+// app.use(history({
+//   disableDotRule: true,
+//   verbose: true
+// }));
+// app.use(staticFileMiddleware);
+
+// app.get('/', function (req, res) {
+//   res.render(path.join(__dirname + '/dist/index.html'));
+// });
+
+// var server = app.listen(process.env.PORT || 8080, function () {
+//   var port = server.address().port;
+//   console.log("App now running on port", port);
+// });
