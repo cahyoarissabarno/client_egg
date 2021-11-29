44a59970c14b15201421d48116a8b106d93045e7
cahyo arissabarno <cahyoarissabarno@gmail.com>
2021-11-30 05:06:57 +0700

    fix route history

--- server.js
+++ server.js
@@ -1,39 +1,11 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
-const history = require('connect-history-api-fallback');
+const history = require('connect-history-api-fallback')
 
-app = express();
+const app = express();
+app.use(history())
 app.use(serveStatic(path.join(__dirname, 'dist')));
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
 
 const port = process.env.PORT || 3000;
 app.listen(port);
-
-// const express = require('express');
-// const serveStatic = require("serve-static")
-// const path = require('path');
-// const history = require('connect-history-api-fallback');
-
-// const app = express();
-
-// const staticFileMiddleware = serveStatic(path.join(__dirname + 'dist'));
-
-// app.use(staticFileMiddleware);
-// app.use(history({
-//   disableDotRule: true,
-//   verbose: true
-// }));
-// app.use(staticFileMiddleware);
-
-// app.get('/', function (req, res) {
-//   res.render(path.join(__dirname + '/dist/index.html'));
-// });
-
-// var server = app.listen(process.env.PORT || 8080, function () {
-//   var port = server.address().port;
-//   console.log("App now running on port", port);
-// });
